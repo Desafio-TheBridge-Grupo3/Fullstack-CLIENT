bf1b0fbd3107f2e17381b5cb0d6bd24f9eae18f3
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-10 13:14:18 +0100

    cpmponentes vacios para ciber

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div>Aquí iría la barra de navegación</div>;
 };
 
 export default Header;
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Oferta from './Oferta'
+import Cosa from './Cosa'
+import Prueba from './Prueba'
+import Vacio from './Vacio'
 
 const Main = () => {
   return (
     <>
       <div>
-        Main
+        Esta será la página de main donde se renderizan todos los componentes
       </div>
       <Oferta />
+      <Cosa />
+      <Prueba />
+      <Vacio />
     </>
   )
 };
