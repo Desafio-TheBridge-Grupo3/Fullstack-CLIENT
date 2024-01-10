25e28d4bc7175470fe2b792c2be3a6a12fe9a9da
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-10 23:59:16 +0100

    librería y carcasa

--- client/src/components/Main/TablaSeveral/TablaSeveral.jsx
+++ client/src/components/Main/TablaSeveral/TablaSeveral.jsx
@@ -3,6 +3,7 @@ import Franja from '../TablaCliente/Franja';
 import SEnergia from './SEnergia';
 import SPotencia from './SPotencia';
 import Form from './Form';
+import Charts from './Charts'
 
 const TablaSeveral = () => {
   return (
@@ -10,6 +11,7 @@ const TablaSeveral = () => {
       <section className="tablaGrande">
         <h1 className="tituloTabla">Propuesta Several</h1>
         <Form/>
+        <Charts/>
         <article className="tabla">
           <Franja />
           <SEnergia />
