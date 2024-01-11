20dc3428d8ff1fe28aea543892686cb23c8c77f7
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-11 17:51:39 +0100

    algunos ajustes de tamaño

--- client/src/components/Main/TablaCliente/TablaCliente.jsx
+++ client/src/components/Main/TablaCliente/TablaCliente.jsx
@@ -9,9 +9,9 @@ const TablaCliente = () => {
   return (
     <>
       <section className="tablaGrande">
+      <article className="compañia" >
         <h1 className="tituloTabla">Compañía actual</h1>
-        <article>
-          <select className="compañia" name="compañia" id="compañia">
+          <select name="compañia" id="compañia">
             <option value="endesa">Endesa</option>
             <option value="iberdrola">Iberdrola</option>
             <option value="naturgy">Naturgy</option>
--- client/src/components/Main/TablaSeveral/Form/Form.jsx
+++ client/src/components/Main/TablaSeveral/Form/Form.jsx
@@ -22,7 +22,11 @@ const Form = () => {
           <label htmlFor="tipo">Tipo de sistema</label>
         </div>
         <select name="tipo" id="tipo" className="select">
-          <option value=""></option>
+          <option value="null"></option>
+          <option value="peninsula">Península</option>
+          <option value="baleares">Baleares</option>
+          <option value="canarias">Canarias</option>
+
         </select>
       </article>
 
@@ -31,7 +35,11 @@ const Form = () => {
           <label htmlFor="tarifa">Tipo de sistema</label>
         </div>
         <select name="tarifa" id="tarifa" className="select">
-          <option value=""></option>
+        <option value="null"></option>
+          <option value="20">2.0TD</option>
+          <option value="30">3.0TD</option>
+          <option value="61">6.1TD</option>
+          <option value="62">6.2TD</option>
         </select>
       </article>
 
@@ -40,34 +48,46 @@ const Form = () => {
           <label htmlFor="cia">Tipo de sistema</label>
         </div>
         <select name="cia" id="cia" className="select">
-          <option value=""></option>
+        <option value="null"></option>
+          <option value="acciona">ACCIONA</option>
+          <option value="adi">ADI</option>
+          <option value="aeq">AEQ</option>
+          <option value="candela">CANDELA</option>
+          <option value="eleia">ELEIA</option>
+          <option value="endesa">ENDESA</option>
+          <option value="evolve">EVOLVE</option>
+          <option value="factor">FACTOR</option>
+          <option value="gana">GANA</option>
+          <option value="iberdrola">IBERDROLA</option>
+          <option value="ignis">IGNIS</option>
+          <option value="max">MAX</option>
+          <option value="naturgy">NATURGY</option>
+          <option value="plenitude">PLENITUDE</option>
+          <option value="total">TOTAL</option>
         </select>
       </article>
 
-      <article>
-        <div className="label">
-          <label htmlFor="cia">Tipo de sistema</label>
-        </div>
-        <select name="cia" id="cia" className="select">
-          <option value=""></option>
-        </select>
-      </article>
 
       <article>
         <div className="label">
           <label htmlFor="metodo">Tipo de sistema</label>
         </div>
         <select name="metodo" id="metodo" className="select">
-          <option value=""></option>
+        <option value="null"></option>
+          <option value="indexado">INDEXADO</option>
+          <option value="fijo">FIJO</option>
         </select>
       </article>
 
       <article>
         <div className="label">
-          <label htmlFor="productos">Tipo de sistema</label>
+          <label htmlFor="productos">Producto CIA (POT)</label>
         </div>
         <select name="productos" id="productos" className="select">
-          <option value=""></option>
+        <option value="null"></option>
+          <option value="armonia">ARMONÍA</option>
+          <option value="equilibrio">EQUILIBRIO</option>
+          <option value="simetria">SIMETRÍA</option>
         </select>
       </article>
 
@@ -76,6 +96,7 @@ const Form = () => {
           <label htmlFor="mes">Tipo de sistema</label>
         </div>
         <select name="mes" id="mes" className="select">
+        <option value="null"></option>
           <option value="en">Enero</option>
           <option value="feb">Febrero</option>
           <option value="mar">Marzo</option>
@@ -96,7 +117,18 @@ const Form = () => {
           <label htmlFor="fee">Tipo de sistema</label>
         </div>
         <select name="fee" id="fee" className="select">
-          <option value=""></option>
+        <option value="null"></option>
+        <option value="1.5">1.5 %</option>
+        <option value="3">3 %</option>
+        <option value="4">4 %</option>
+        <option value="5">5 %</option>
+        <option value="6">6 %</option>
+        <option value="8">8 %</option>
+        <option value="10">10 %</option>
+        <option value="15">15 %</option>
+        <option value="20">20 %</option>
+        <option value="25">25 %</option>
+        <option value="30">30 %</option>
         </select>
       </article>
 
