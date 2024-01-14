78b9718d0f998b7cd9e8e63081bc52832d62e27d
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-14 09:40:31 +0100

    enrutado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import React, { useState } from 'react';
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
 
 
   return (
     <>
-      <Header />
-      <Main />
+      <BrowserRouter>
+        <Header />
+        <Main />
+      </BrowserRouter>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
