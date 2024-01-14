6f76a40a8cd0eaa11995ed793314c2f140bfda6c
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-14 11:17:03 +0100

    creacion pdf provisional

--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { usePDF } from 'react-to-pdf';
 import TablaCliente from "./TablaCliente";
 import TablaSeveral from "./TablaSeveral";
 
 const Home = () => {
+  const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
+
   return (
-  <>
-  <TablaCliente />
-  <TablaSeveral />
-  </>
+    <>
+      <section>
+        <section ref={targetRef}>
+          <TablaCliente />
+          <TablaSeveral />
+          <button onClick={() => toPDF()}>Download PDF</button>
+        </section>
+      </section>
+    </>
   );
 };
 
