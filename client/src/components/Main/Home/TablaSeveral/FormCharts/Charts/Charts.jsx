95ab003ad3d4c92a632c8f32f6794868cbb90a4c
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-16 13:47:02 +0100

    mini arreglo, no se ve la barra

--- client/src/components/Main/Home/TablaSeveral/FormCharts/Charts/Charts.jsx
+++ client/src/components/Main/Home/TablaSeveral/FormCharts/Charts/Charts.jsx
@@ -3,41 +3,39 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 import { useContext } from "react";
 import { MacroContext } from "../../../../../../context/MacroContext";
 
-
 const Charts = () => {
-
   const { otros } = useContext(MacroContext);
 
-const ahorroActual = (otros.importeTotalFacturaCliente - otros.importeTotalFacturaSeveral)
-const ahorroAnual = (otros.importeAnualEstimadoCliente - otros.importeAnualEstimadoSeveral)
+  const ahorroActual = (otros.importeTotalFacturaCliente - otros.importeTotalFacturaSeveral);
+  const ahorroAnual = (otros.importeAnualEstimadoCliente - otros.importeAnualEstimadoSeveral)
 
-const porcentajeActual = (ahorroActual * 100) / otros.importeTotalFacturaCliente
-const porcentajeAnual = (ahorroAnual * 100) / otros.importeAnualEstimadoCliente
+  const porcentajeActual = (ahorroActual * 100) / otros.importeTotalFacturaCliente
+  const porcentajeAnual = (ahorroAnual * 100) / otros.importeAnualEstimadoCliente
 
-const porcentajeFixed1 = porcentajeActual.toFixed(0)
-const porcentajeFixed2 = porcentajeAnual.toFixed(0)
+  const porcentajeFixed1 = porcentajeActual.toFixed(0)
+  const porcentajeFixed2 = porcentajeAnual.toFixed(0)
 
-return (
-  <>
-    <section id="chartsComponent">
-      <section id="actual">
-        <h3>Ahorro factura actual</h3>
-        <h1>{ahorroActual}</h1>
-        <article id="chart1">
-          <ProgressBar variant='danger' now={porcentajeActual} label={`${porcentajeActual}%`} />
-        </article>
+  return (
+    <>
+      <section id="chartsComponent">
+        <section id="actual">
+          <h3>Ahorro factura actual</h3>
+          <h1>{ahorroActual}</h1>
+          <article id="chart1">
+            <ProgressBar variant='danger' now={porcentajeFixed1} label={`${porcentajeFixed1}%`} />
+          </article>
+        </section>
+
+        <section id="anual">
+          <h3>Ahorro anual</h3>
+          <h1>{ahorroAnual}</h1>
+          <article id="chart2">
+            <ProgressBar variant='danger' now={porcentajeFixed2} label={`${porcentajeFixed2}%`} />
+          </article>
+        </section>
       </section>
-
-      <section id="anual">
-        <h3>Ahorro anual</h3>
-        <h1>{ahorroAnual}</h1>
-        <article id="chart2">
-          <ProgressBar variant='danger' now={porcentajeAnual} label={`${porcentajeAnual}%`} />
-        </article>
-      </section>
-    </section>
-  </>
-);
+    </>
+  );
 };
 
 export default Charts;
