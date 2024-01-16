9681f0153be4a916178aae3a8596aaade9ee7b15
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-01-16 13:17:57 +0100

    deleted non working code

--- client/src/components/Main/CargaDatos/CargaDatos.jsx
+++ client/src/components/Main/CargaDatos/CargaDatos.jsx
@@ -1,30 +1,45 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
 
 
 
 const CargaDatos = () => {
 
   const navigate = useNavigate()
+
   const rellenarTabla=()=>{
     navigate(`/home`)
   }
 
   const [selectedFile, setSelectedFile] = useState(null);
 
+  const handleFile = (event) => {
+    const archivo = event.target.files[0];
+    setSelectedFile(archivo);
+  };  
+
+  const deshabilitado=()=>{
+    console.log("Este botón está deshabilitado, sube un archivo")
+  }
+
 
   return (
+    <>
+    <article className="carga">
     <article id="metodo">
       <h2 id="pregunta">¿Cómo quieres cargar los datos?</h2>
       
-      <button className="opciones" onClick={rellenarTabla}>
-        <input type="file" value={selectedFile} onChange={(e) => setSelectedFile(e.target.files[0])}
+      <button className="opciones" >
+        <input type="file" value={selectedFile}
         />Subir factura</button>
       <p id="mini">Adjunta aquí tu factura. Formatos aceptados: .pdf y .jpg</p>
-      <p>Rellenar datos manualmente</p>
+      <p id="cambio" onClick={rellenarTabla}>Rellenar datos manualmente</p>
     </article>
+
+    <button className={selectedFile ? 'boton-con-archivo' : 'boton-sin-archivo'} onClick={selectedFile? rellenarTabla : deshabilitado }>Continuar</button>
+    </article>
+    </>
   );
 };
 
