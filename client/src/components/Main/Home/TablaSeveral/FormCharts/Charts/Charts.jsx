9780e121ea409a5c90a87ec46463ba945299a49c
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-16 13:01:04 +0100

    maquetación

--- client/src/components/Main/Home/TablaSeveral/FormCharts/Charts/Charts.jsx
+++ client/src/components/Main/Home/TablaSeveral/FormCharts/Charts/Charts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import { useContext } from "react";
-import { MacroContext } from "../../../../../context/MacroContext";
+import { MacroContext } from "../../../../../../context/MacroContext";
 
 
 const Charts = () => {
@@ -9,27 +10,30 @@ const Charts = () => {
 
 const ahorroActual = (otros.importeTotalFacturaCliente - otros.importeTotalFacturaSeveral);
 const ahorroAnual = (otros.importeAnualEstimadoCliente - otros.importeAnualEstimadoSeveral)
+const porcentajeActual =0
+const porcentajeAnual =0
 
-
-  return (
-    <>
+return (
+  <>
+    <section id="chartsComponent">
       <section id="actual">
         <h3>Ahorro factura actual</h3>
-        <article id="chart1">
-        </article>
         <h1>{ahorroActual}</h1>
+        <article id="chart1">
+          <ProgressBar variant='danger' now={porcentajeActual} label={`${porcentajeActual}%`} />;
+        </article>
       </section>
 
       <section id="anual">
         <h3>Ahorro anual</h3>
-        <article id="chart2">
-
-        </article>
-
         <h1>{ahorroAnual}</h1>
+        <article id="chart2">
+          <ProgressBar variant='danger' now={porcentajeAnual} label={`${porcentajeAnual}%`} />;
+        </article>
       </section>
-    </>
-  )
+    </section>
+  </>
+);
 };
 
 export default Charts;
