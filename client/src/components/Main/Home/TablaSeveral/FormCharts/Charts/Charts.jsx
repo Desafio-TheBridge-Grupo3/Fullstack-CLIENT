53d7d9a758fa5655f282f698bf3f153f5125c741
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-16 13:41:44 +0100

    falta que se vea la barra

--- client/src/components/Main/Home/TablaSeveral/FormCharts/Charts/Charts.jsx
+++ client/src/components/Main/Home/TablaSeveral/FormCharts/Charts/Charts.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import ProgressBar from "react-bootstrap/ProgressBar";
+import React from "react";
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import { useContext } from "react";
 import { MacroContext } from "../../../../../../context/MacroContext";
 
@@ -8,10 +8,14 @@ const Charts = () => {
 
   const { otros } = useContext(MacroContext);
 
-const ahorroActual = (otros.importeTotalFacturaCliente - otros.importeTotalFacturaSeveral);
+const ahorroActual = (otros.importeTotalFacturaCliente - otros.importeTotalFacturaSeveral)
 const ahorroAnual = (otros.importeAnualEstimadoCliente - otros.importeAnualEstimadoSeveral)
-const porcentajeActual =0
-const porcentajeAnual =0
+
+const porcentajeActual = (ahorroActual * 100) / otros.importeTotalFacturaCliente
+const porcentajeAnual = (ahorroAnual * 100) / otros.importeAnualEstimadoCliente
+
+const porcentajeFixed1 = porcentajeActual.toFixed(0)
+const porcentajeFixed2 = porcentajeAnual.toFixed(0)
 
 return (
   <>
@@ -20,7 +24,7 @@ return (
         <h3>Ahorro factura actual</h3>
         <h1>{ahorroActual}</h1>
         <article id="chart1">
-          <ProgressBar variant='danger' now={porcentajeActual} label={`${porcentajeActual}%`} />;
+          <ProgressBar variant='danger' now={porcentajeActual} label={`${porcentajeActual}%`} />
         </article>
       </section>
 
@@ -28,7 +32,7 @@ return (
         <h3>Ahorro anual</h3>
         <h1>{ahorroAnual}</h1>
         <article id="chart2">
-          <ProgressBar variant='danger' now={porcentajeAnual} label={`${porcentajeAnual}%`} />;
+          <ProgressBar variant='danger' now={porcentajeAnual} label={`${porcentajeAnual}%`} />
         </article>
       </section>
     </section>
