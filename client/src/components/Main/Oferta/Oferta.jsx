a41300354092c2952385aeb2b21a5b6f2d67686f
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-01-17 13:54:25 +0100

    styling finished, logic pending

--- client/src/components/Main/Oferta/Oferta.jsx
+++ client/src/components/Main/Oferta/Oferta.jsx
@@ -32,28 +32,115 @@ const Oferta = () => {
               </article>
             </section>
             <section id="topRight">
-            <h1 id="graphTitle">Ahorro con propuesta Several</h1>
+              <h1 id="graphTitle">Ahorro con propuesta Several</h1>
               <article id="graphContainer">
                 <div className="factura">
                   <h2 className="mTitle">Factura actual</h2>
                   <h1 className="proposal">4.789,86€</h1>
-                  <ProgressBar completed={50} bgColor="#C1240C" baseBgColor="#EBD7D8"/>
+                  <ProgressBar
+                    completed={50}
+                    bgColor="#C1240C"
+                    baseBgColor="#EBD7D8"
+                  />
                 </div>
                 <div className="factura">
                   <h2 className="mTitle">Factura anual</h2>
                   <h1 className="proposal">4.789,86€</h1>
-                  <ProgressBar completed={50} bgColor="#C1240C" baseBgColor="#EBD7D8"/>
+                  <ProgressBar
+                    completed={50}
+                    bgColor="#C1240C"
+                    baseBgColor="#EBD7D8"
+                  />
                 </div>
               </article>
             </section>
           </section>
+
           <section id="middle">
             <article id="middleTitle">
               <h1 className="mTitle">Datos de Factura actual</h1>
               <h3 className="mTitle2">Días de facturación: 37</h3>
             </article>
-            <article id="middleTabla"></article>
-            <article id="middleTotal">
+            <article id="middleTabla">
+              <table className="facturaActual">
+                <tr>
+                  <th> </th>
+                  <th>P1</th>
+                  <th>P2</th>
+                  <th>P3</th>
+                  <th>P4</th>
+                  <th>P5</th>
+                  <th>P6</th>
+                </tr>
+                <tr>
+                  <td className="typeInput">Precios potencia (€/kWh/dia)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">
+                    Precios energía media anual (€/kWh)
+                  </td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">Precios energía (€/kWh)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">Consumo anual estimado (Kwh)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">Consumo factura actual (Kwh)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">Potencia facturada (Kw)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+              </table>
+              <table className="conclusionesFactura">
+                <th className="typeInput">Energía reactiva</th>
+                <th className="inputPeriodo">00,0 €</th>
+                <th className="typeInput">Impuesto eléctrico</th>
+                <th className="inputPeriodo">00,0 €</th>
+                <th className="typeInput">Alquiler de equipo</th>
+                <th className="inputPeriodo">00,0 €</th>
+                <th className="typeInput">Otros conceptos</th>
+                <th className="inputPeriodo">00,0 €</th>
+              </table>
+            </article>
+            <article className="total">
               <div className="totalContainer">
                 <h3 className="totalTitulo">Total factura</h3>
                 <h3 className="totalNum">xxx,xx €</h3>
@@ -64,12 +151,69 @@ const Oferta = () => {
               </div>
             </article>
           </section>
-          <section id="bottom">
-            <article id="bottomTitle">
-              <h1 className="mTitle">Datos de Factura actual</h1>
+
+
+          <section id="middle">
+            <article id="middleTitle">
+              <h1 className="mTitle">Propuesta Several</h1>
+              <h3 className="mTitle2">Días de facturación: 37</h3>
             </article>
-            <article id="bottomTabla"></article>
-            <article id="bottomTotal">
+            <article id="middleTabla">
+              <table className="facturaActual">
+                <tr>
+                  <th> </th>
+                  <th>P1</th>
+                  <th>P2</th>
+                  <th>P3</th>
+                  <th>P4</th>
+                  <th>P5</th>
+                  <th>P6</th>
+                </tr>
+                <tr>
+                  <td className="typeInput">Precios potencia (€/kWh/dia)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">
+                    Precios energía media anual (€/kWh)
+                  </td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+                <tr>
+                  <td className="typeInput">Precios energía (€/kWh)</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                  <td className="inputPeriodo">0,000000 €</td>
+                </tr>
+              </table>
+              <table className="conclusionesFactura">
+                <th className="typeInput">Energía reactiva</th>
+                <th className="inputPeriodo">00,0 €</th>
+                <th className="typeInput">Impuesto eléctrico</th>
+                <th className="inputPeriodo">00,0 €</th>
+                <th className="typeInput">Alquiler de equipo</th>
+                <th className="inputPeriodo">00,0 €</th>
+                <th className="typeInput">Otros conceptos</th>
+                <th className="inputPeriodo">00,0 €</th>
+              </table>
+              <article id="aclaracion">
+              La estimación anual ha sido realizada utilizando datos históricos de consumo energético publicados del ultimo año en SIPS y considerando la proyección anual del perfil de consumo así como los precios facilitados por el cliente en su última factura de luz. 
+              </article>
+            </article>
+            <article className="total">
               <div className="totalContainer">
                 <h3 className="totalTitulo">Total factura</h3>
                 <h3 className="totalNum">xxx,xx €</h3>
@@ -79,19 +223,21 @@ const Oferta = () => {
                 <h3 className="totalNum">xx.xxx,xx €</h3>
               </div>
             </article>
-            <article id="bottomTextContainer">
-              <h4>
-                Propuesta valida durante 7 (siete) dias desde la fecha de la
-                propuesta.
-              </h4>
-              <h5>Documentación para efectuar la modificación propuesta:</h5>
-              <h5>
-                (1) DNI Firmante (2) Copia Facturas (3) Recibo Bancario - (4)
-                CIF de la empresa
-              </h5>
-            </article>
           </section>
-          <button onClick={() => toPDF()}>Generar PDF</button>
+          <article id="bottomTextContainer">
+            <h4>
+              Propuesta valida durante 7 (siete) dias desde la fecha de la
+              propuesta.
+            </h4>
+            <h5>Documentación para efectuar la modificación propuesta:</h5>
+            <h5>
+              (1) DNI Firmante (2) Copia Facturas (3) Recibo Bancario - (4) CIF
+              de la empresa
+            </h5>
+          </article>
+          <article className="btnContainer">
+          <button className="btnPDF" onClick={() => toPDF()}>DESCARGAR PDF</button>
+            </article>        
         </main>
       </div>
     </>
